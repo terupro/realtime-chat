8f5b4dfb82c37dde6aea51ac3afbd9b6f1c4013a
‘terupro’ <terupro911@gmail.com>
2022-08-15 21:03:12 +0900

    ✨ メッセージの表示

--- client/src/App.js
+++ client/src/App.js
@@ -8,32 +8,41 @@ const socket = io("http://localhost:4000");
 function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {
     if (username !== "" && room !== "") {
       console.log(username);
       console.log(room);
       socket.emit("join_room", room);
+      setShowChat(true);
     }
   };
 
   return (
     <div className="App">
-      <div className="joinChatContainer">
-        <h3>チャットに参加する</h3>
-        <input
-          type="text"
-          placeholder="お名前"
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="ルーム番号"
-          onChange={(e) => setRoom(e.target.value)}
-        />
-        <button onClick={joinRoom}>ルームに参加</button>
+      <div className="filter">
+        <h1>RealTime Chat</h1>
+        {!showChat ? (
+          <div className="joinChatContainer">
+            <h3>チャットに参加しよう！</h3>
+            <p>同じルーム番号の人とチャットができるよ</p>
+            <input
+              type="text"
+              placeholder="お名前"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="ルーム番号"
+              onChange={(e) => setRoom(e.target.value)}
+            />
+            <button onClick={joinRoom}>ルームに参加</button>
+          </div>
+        ) : (
+          <Chat socket={socket} username={username} room={room} />
+        )}
       </div>
-      <Chat socket={socket} username={username} room={room} />
     </div>
   );
 }
--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
 
 const Chat = ({ socket, username, room }) => {
   const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
 
   const sendMessage = async () => {
     if (currentMessage !== "") {
@@ -15,11 +17,14 @@ const Chat = ({ socket, username, room }) => {
           new Date(Date.now()).getMinutes(),
       };
       await socket.emit("send_message", messageData);
+      setMessageList([...messageList, messageData]);
+      setCurrentMessage("");
     }
   };
 
   socket.on("receive_message", (data) => {
     console.log(data);
+    setMessageList([...messageList, data]);
   });
 
   return (
@@ -27,11 +32,31 @@ const Chat = ({ socket, username, room }) => {
       <div className="chat-header">
         <p>ライブチャット</p>
       </div>
-      <div className="chat-body"></div>
+      <div className="chat-body">
+        <ScrollToBottom className="message-container">
+          {messageList.map((messageContent) => (
+            <div
+              className="message"
+              id={username == messageContent.author ? "you" : "other"}
+            >
+              <div>
+                <div className="message-content">
+                  <p>{messageContent.message}</p>
+                </div>
+                <div className="message-meta">
+                  <p id="time">{messageContent.time}</p>
+                  <p id="author">{messageContent.author}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </ScrollToBottom>
+      </div>
       <div className="chat-footer">
         <input
           type="text"
           placeholder="メッセージ内容"
+          value={currentMessage}
           onChange={(e) => setCurrentMessage(e.target.value)}
         />
         <button onClick={sendMessage}>&#9658;</button>
